09be47ad163062a7a60291ee51846ff5445391a1
Yuri Gorokhov <yurigorokhov@gmail.com>
2016-09-14 12:11:23 -0700

    Adding solution

--- MagicNightRoslyn/MagicNightRoslyn/DiagnosticAnalyzer.cs
+++ MagicNightRoslyn/MagicNightRoslyn/DiagnosticAnalyzer.cs
@@ -1,4 +1,6 @@
 using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -36,14 +38,75 @@ namespace MagicNightRoslyn {
         }
 
         private static void AnalyzeInvocation(SyntaxNodeAnalysisContext context) {
-            
-            //TODO: implement your analysis!
+            var invocationNode = (InvocationExpressionSyntax)context.Node;
+            var symbol = context.SemanticModel.GetSymbolInfo(invocationNode).Symbol as IMethodSymbol;
+            if(symbol == null) {
+                return;
+            }
+            var attributes = symbol.GetAttributes();
+            if(!attributes.Any()) {
+                return;
+            }
+            var formatAttribute = attributes.FirstOrDefault(attr => attr.AttributeClass.OriginalDefinition.ContainingAssembly.Name == "StringFormatAttribute"
+                                                                    && attr.AttributeClass.OriginalDefinition.ContainingNamespace.Name == "StringFormatAttribute"
+                                                                    && attr.AttributeClass.OriginalDefinition.Name == "StringFormat");
+            if(formatAttribute == null) {
+                return;
+            }
+            var args = formatAttribute.ConstructorArguments.ToArray();
+            var formatArg = args[0].Value;
+            var argsArg = args[1].Value;
 
-            // Rough guideline:
-            // 1) Find out what method is being invoked
-            // 2) Find out if that method is marked with out special StringFormat attribute
-            // 3) Figure out which parameters correspond to the format string and arguments
-            // 4) Validate the format string and arguments
+            // check the method declaration to see which parameters are the ones we care about positionally
+            var parameters = symbol.Parameters;
+            int formatIdx = -1;
+            int argsIdx = -1;
+            for(var i = 0; i < parameters.Length; i++) {
+                if(formatArg.Equals(parameters[i].Name)) {
+                    formatIdx = i;
+                }
+                if(argsArg.Equals(parameters[i].Name)) {
+                    argsIdx = i;
+                }
+            }
+            if(formatIdx == -1) {
+                ReportDiagnostic(context, invocationNode.GetLocation(), "Parameter not found", $"Parameter {formatArg} was not found");
+                return;
+            }
+            if(argsIdx == -1) {
+                ReportDiagnostic(context, invocationNode.GetLocation(), "Parameter not found", $"Parameter {argsArg} was not found");
+                return;
+            }
+
+            // check the invocation to see what it is being called with!
+            var formatArgument = invocationNode.ArgumentList.Arguments[formatIdx];
+            var argsArgument = invocationNode.ArgumentList.Arguments.Skip(argsIdx).ToArray();
+            
+            // we have found a format attribute, let's investigate the parameters!
+            if(formatArgument.Expression.IsKind(SyntaxKind.StringLiteralExpression)) {
+                var formatString = formatArgument.Expression as LiteralExpressionSyntax;
+                ValidateFormatString(formatString.Token.ValueText, argsArgument, context, invocationNode.GetLocation());
+            }
+        }
+
+        private static void ValidateFormatString(string format, ArgumentSyntax[] args, SyntaxNodeAnalysisContext context, Location location) {
+            var matches = Regex.Matches(format, "{[0-9]*}");
+            if(args.Length != matches.Count) {
+                ReportDiagnostic(context, location, "Invalid number of parameters", $"The number of parameters does not match the number of arguments");
+            }
+        }
+
+        private static void ReportDiagnostic(SyntaxNodeAnalysisContext context, Location location, string title, string messageFormat) {
+            var diagnostic = Diagnostic.Create(new DiagnosticDescriptor(
+                DiagnosticId,
+                title,
+                messageFormat,
+                Category,
+                DiagnosticSeverity.Error,
+                isEnabledByDefault: true,
+                description: Description
+            ), location, "");
+            context.ReportDiagnostic(diagnostic);
         }
     }
 }
